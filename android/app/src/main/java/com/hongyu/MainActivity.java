64914d46ecf55a76563c7d9a5817da82764e1119
hyeju1123 <hjjun1123@gmail.com>
2023-11-06 15:19:41 +0900

    install react-navigation and configure MainActivity.java

--- android/app/src/main/java/com/hongyu/MainActivity.java
+++ android/app/src/main/java/com/hongyu/MainActivity.java
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// react-native-screens package requires one additional configuration step to properly work on Android devices.
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -28,5 +31,13 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  /**
+   * react-native-screens package requires one additional configuration step to properly work on Android devices.
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
