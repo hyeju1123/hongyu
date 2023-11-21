443bd98c63738d996da8306641888000d62e6909
hyeju1123 <hjjun1123@gmail.com>
2023-11-21 10:54:31 +0900

    upload png images and set path

---
+++ src/styles/images.ts
@@ -0,0 +1,36 @@
+const images = {
+  module: {
+    arrow: require('../../images/arrow.png'),
+    edit: require('../../images/edit.png'),
+    editWhite: require('../../images/editWhite.png'),
+    lanternOff: require('../../images/lanternOff.png'),
+    lanternOffWhite: require('../../images/lanternOffWhite.png'),
+    lanternOn: require('../../images/lanternOn.png'),
+    mainLantern: require('../../images/mainLantern.png'),
+    pencilWithZh: require('../../images/pencilWithZh.png'),
+    radicalDict: require('../../images/radicalDict.png'),
+    sound: require('../../images/sound.png'),
+    vocatest: require('../../images/vocatest.png'),
+    search: require('../../images/search.png'),
+    dumpling: require('../../images/dumpling.png'),
+    checked: require('../../images/checked.png'),
+  },
+  wordClass: {
+    명사: require('../../images/wordClass/noun.png'),
+    대명사: require('../../images/wordClass/pronoun.png'),
+    동사: require('../../images/wordClass/verb.png'),
+    형용사: require('../../images/wordClass/adjective.png'),
+    부사: require('../../images/wordClass/adverb.png'),
+    개사: require('../../images/wordClass/preposition.png'),
+    접속사: require('../../images/wordClass/conjunction.png'),
+    양사: require('../../images/wordClass/quantifier.png'),
+    조사: require('../../images/wordClass/postposition.png'),
+    조동사: require('../../images/wordClass/auxiliary.png'),
+    감탄사: require('../../images/wordClass/exclamation.png'),
+    성어: require('../../images/wordClass/idiom.png'),
+    수사: require('../../images/wordClass/numeral.png'),
+    기타: require('../../images/wordClass/other.png'),
+  },
+} as {[key: string]: any};
+
+export default images;
