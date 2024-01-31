72bc45f75f76e36a93f75a3179e425e530a766d0
hyeju1123 <hjjun1123@gmail.com>
2024-01-31 10:53:48 +0900

    add react-native-gesture-handler lib

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {RealmProvider} from './RealmConfigContext';
 import {RecoilRoot} from 'recoil';
-import Toast from './src/module/Toast';
-import RootNavigation from './src/navigation/RootNavigation';
 import {VocaProvider} from './src/providers/VocaProvider';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import RootNavigation from './src/navigation/RootNavigation';
+import Toast from './src/module/Toast';
+
+import styles from './src/styles/main/MainPageStyle';
 
 function App(): JSX.Element {
   return (
     <RealmProvider>
       <RecoilRoot>
         <VocaProvider>
-          <RootNavigation />
-          <Toast />
+          <GestureHandlerRootView style={styles.container}>
+            <RootNavigation />
+            <Toast />
+          </GestureHandlerRootView>
         </VocaProvider>
       </RecoilRoot>
     </RealmProvider>
